0875b506e762877921a0346fca4602d0f6190a84
hasankayman <hasankayman@gmail.com>
2020-07-25 09:45:09 -0600

    Day 33

--- src/Library/Util.java
+++ src/Library/Util.java
@@ -137,5 +137,44 @@ public class Util {
     }
         // inserts the element to the array
 
+    public static int[] sortArrayDescending(int[] array) {
+        int[] reversed = new int[array.length];
+        int i = array.length - 1;
+        for (int each : array) {
+            reversed[i] = each;
+            i--;
+        }
+        return reversed;
+    }
+
+    public static double[] sortArrayDescending(double[] array) {
+        double[] reversed = new double[array.length];
+        int i = array.length - 1;
+        for (double each : array) {
+            reversed[i] = each;
+            i--;
+        }
+        return reversed;
+    }
+
+    public static String[] sortArrayDescending(String[] array) {
+        String[] reversed = new String[array.length];
+        int i = array.length - 1;
+        for (String each : array) {
+            reversed[i] = each;
+            i--;
+        }
+        return reversed;
+    }
+
+    public static char[] sortArrayDescending(char[] array) {
+        char[] reversed = new char[array.length];
+        int i = array.length - 1;
+        for (char each : array) {
+            reversed[i] = each;
+            i--;
+        }
+        return reversed;
+    }
 
 }
