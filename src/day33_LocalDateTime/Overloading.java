f0332b255ab3954c869c76309f91d0d9cc8d5790
hasankayman <hasankayman@gmail.com>
2020-07-25 10:48:40 -0600

    Day 33

---
+++ src/day33_LocalDateTime/Overloading.java
@@ -0,0 +1,25 @@
+package day33_LocalDateTime;
+
+import java.util.Scanner;
+
+import Library.Util;
+
+public class Overloading {
+    public static String mainmethod(String[] args) {
+        System.out.println("String array");
+        return null;
+    }
+
+    public static void main(int[] args) {
+        System.out.println("int array");
+
+    }
+
+    public static void main(double[] args) {
+        System.out.println("double array");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
