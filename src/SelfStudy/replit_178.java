b5319707a83110bf6a3b7671883b71b3009a71ff
hasankayman <hasankayman@gmail.com>
2020-07-25 20:20:30 -0600

    Self Study

--- src/SelfStudy/replit_178.java
+++ src/SelfStudy/replit_178.java
@@ -2,19 +2,19 @@ package SelfStudy;
 
 public class replit_178 {
     public static String mergeStrings(String one, String two) {
-        String result = "";
-        for (int i = 0; i < one.length() || i <two.length(); i++) {
-           if(i < one.length()) {
-               result += one.charAt(i);
-           }
-           if(i < two.length()) {
-               result += two.charAt(i);
-           }
+        String newword = "";
+        for (int i = 0; i < one.length() || i < two.length(); i++) {
+            if (i < one.length()) {
+                newword += one.charAt(i);
+            }
+            if (i < two.length()) {
+                newword += two.charAt(i);
+            }
         }
-        return result;
+        return newword;
     }
 
     public static void main(String[] args) {
-        System.out.println(mergeStrings("merve", "selim"));
+        System.out.println(mergeStrings("kazim", "orhan"));
     }
 }
