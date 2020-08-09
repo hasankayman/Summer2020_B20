15e9b5f764a764cbf4132f921a5f5fa0064b621f
hasankayman <hasankayman@gmail.com>
2020-08-09 15:11:47 -0600

    Day 39 class

---
+++ src/day39_CustomClass/BankAccount.java
@@ -0,0 +1,17 @@
+package day39_CustomClass;
+
+public class BankAccount {
+    /*
+    Attributes : accountName, accountNumber, accountBalance
+
+    Actions : withdraw, deposit, checkAvailableAccount
+    */
+    String accountName;
+    long accountNumber;
+    double accountBalance;
+
+    public void accountWithdraw(){
+
+    }
+
+}
