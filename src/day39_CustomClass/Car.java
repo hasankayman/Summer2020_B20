aa0932ec287b835e4705c1fb9773dfb0922cf628
hasankayman <hasankayman@gmail.com>
2020-08-09 10:44:48 -0600

    Day 39 class

--- src/day39_CustomClass/Car.java
+++ src/day39_CustomClass/Car.java
@@ -13,5 +13,17 @@ public class Car {
     double mileage;
     double price;
 
+    public void setInfo(String carBrand, String carModel, int carYear, String carColor, double carMileage, double carPrice) {
+        brand = carBrand;
+        model = carModel;
+        year = carYear;
+        color = carColor;
+        mileage = carMileage;
+        price = carPrice;
+    }
+    public void getInfo(){
+        System.out.println(year + " " + brand + " " + model + ", " + color + ", " + mileage + ", $" + price);
+    }
+
 
 }
--- src/day39_CustomClass/CarObject.java
+++ src/day39_CustomClass/CarObject.java
@@ -3,12 +3,15 @@ package day39_CustomClass;
 public class CarObject {
     public static void main(String[] args) {
         Car car1 = new Car();
-        car1.brand = "Toyota";
+        /*car1.brand = "Toyota";
         car1.model = "Avalon";
         car1.year = 2020;
         car1.color = "White";
         car1.mileage = 20000;
-        car1.price = 19000.50;
+        car1.price = 19000.50;*/
+
+        car1.setInfo("Toyota", "Avalon", 2014, "Red",55000,16000);
+
 
         System.out.println(car1.brand);
         System.out.println(car1.model);
@@ -17,13 +20,27 @@ public class CarObject {
 
         System.out.println("==========================");
         Car car2 = new Car();
-        car2.brand = "Mercedes";
+        /*car2.brand = "Mercedes";
         car2.model = "G 550";
         car2.year = 2000;
-        car2.color = "Blue";
+        car2.color = "Blue";*/
+        car2.setInfo("BMW","X5",2019,"Black",2000,24999.99);
         System.out.println(car2.brand);
         System.out.println(car2.model);
         System.out.println(car2.year);
         System.out.println(car2.color);
+
+        Car car3 = new Car();
+        car3.setInfo("Audi", "A7", 2017, "While", 32000, 25000);
+
+        System.out.println("**********************************");
+
+        Car car4 = new Car();
+        car4.setInfo("Jeep","Wramgler", 1990, "Blue", 250000, 127000);
+        car4.getInfo();
+        car3.getInfo();
+        car2.getInfo();
+        car1.getInfo();
+
     }
 }
