7aaddb190a7d9b8b5a5310f8477077478ef0b8b9
hasankayman <hasankayman@gmail.com>
2020-08-09 09:00:28 -0600

    Day 39 class

---
+++ src/day39_CustomClass/Car.java
@@ -0,0 +1,17 @@
+package day39_CustomClass;
+
+public class Car {
+    /*
+     Attributes:
+     model, brand, year, color, mileage, price ...
+    */
+
+    String brand;
+    String model;
+    int year;
+    String color;
+    double mileage;
+    double price;
+
+
+}
