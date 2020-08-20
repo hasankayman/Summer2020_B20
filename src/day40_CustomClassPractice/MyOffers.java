f9351a69febb7580704eab8fd9632b3ce667a20d
hasankayman <hasankayman@gmail.com>
2020-08-19 18:22:47 -0600

    Day 44 class

--- src/day40_CustomClassPractice/MyOffers.java
+++ src/day40_CustomClassPractice/MyOffers.java
@@ -1,9 +1,10 @@
 package day40_CustomClassPractice;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyOffers {
+
     public static void main(String[] args) {
         Offer offer1 = new Offer();
         Offer offer2 = new Offer();
@@ -13,7 +14,7 @@ public class MyOffers {
         Offer offer6 = new Offer();
         Offer offer7 = new Offer();
 
-        offer1.setOfferInfo(100000, "VA", true, true, true, "SDET", true);
+        offer1.setOfferInfo(105000, "VA", true, true, true, "SDET", true);
         offer2.setOfferInfo(175_000,"VA",true,true, false, "SDET", true);
         offer3.setOfferInfo(125000,"VA", true, true, true, "SDET", true);
         offer4.setOfferInfo(130_000,"PA",true,false, false, "QA", true);
@@ -25,21 +26,29 @@ public class MyOffers {
         ArrayList<Offer> jobOffers = new ArrayList<>();
         jobOffers.addAll(Arrays.asList(offer1, offer2, offer3, offer4, offer5, offer6, offer7));
 
-
-        for (Offer eachoffer : jobOffers) {
-            eachoffer.getOfferInfo();
+        for( Offer eachOffer  : jobOffers){
+            eachOffer.getOfferInfo();
         }
 
-        /*jobOffers.removeIf(p -> p.salary < 120000);
-        System.out.println("Number of offers : " + jobOffers.size());
-*/
-        /*String myLocation = "VA";
-        jobOffers.removeIf(p -> p.salary < 120000 || !p.state.equals(myLocation));
-        System.out.println("Number of offers: " + jobOffers.size());*/
+        System.out.println("==================================================");
+        // less than 120k:
+      //   jobOffers.removeIf( p -> p.salary < 120000  );
+     //   System.out.println("Number of offers: "+jobOffers.size());
 
-        //does not have PTO or does not have benefit or is not fulltime
-        jobOffers.removeIf(p -> p.hasPTO == false || p.hasBenefit == false || p.isFullTime == false);
-        System.out.println("Number of offers: " + jobOffers.size());
+
+        // less than 120 or not from VA area
+      //  jobOffers.removeIf( p -> p.salary< 120000 || !p.state.equals("VA") );
+      //  System.out.println("Number of Offers: "+jobOffers.size());
+
+
+        //  does not have PTO or does not have benefit or is not full time, or less than 120k
+        jobOffers.removeIf( p -> p.hasPTO == false || p.hasBenefit == false || p.isFullTime == false || p.salary < 120000);
+        System.out.println("Number of Offers: "+jobOffers.size());
+
+
+
+
 
     }
+
 }
--- src/day40_CustomClassPractice/Offer.java
+++ src/day40_CustomClassPractice/Offer.java
@@ -1,6 +1,18 @@
 package day40_CustomClassPractice;
+/*
+Offer
+		Attribute:
+			salary, Location, hasPTO, isFullTime, WFH, jobTitle, hasBenefit
+
+		Actions:
+			setOfferInfo
+			getOfferInfo
+
+		MUST use this keyword
+ */
 
 public class Offer {
+
     double salary;
     String state;
     boolean hasPTO;
@@ -8,8 +20,10 @@ public class Offer {
     boolean isWFH;
     String jobTitle;
     boolean hasBenefit;
-    public void setOfferInfo(double salary, String state, boolean hasPTO, boolean isFullTime, boolean isWFH, String jobTitle, boolean hasBenefit) {
-        this.salary = salary;
+
+
+    public void setOfferInfo(double salary, String state, boolean hasPTO, boolean isFullTime, boolean isWFH, String jobTitle, boolean hasBenefit){
+        this.salary  = salary;
         this.state = state;
         this.hasPTO = hasPTO;
         this.isFullTime = isFullTime;
@@ -17,14 +31,16 @@ public class Offer {
         this.jobTitle = jobTitle;
         this.hasBenefit = hasBenefit;
     }
-    public void getOfferInfo() {
-        System.out.println("================================================");
-        System.out.println("Salary: " + salary );
-        System.out.println("Job Title: " + jobTitle);
-        System.out.println("Location: " + state);
 
-        System.out.println("================================================");
 
+    public void getOfferInfo(){// display the offer info
+        System.out.println("==============================================");
+        System.out.println("Salary: "+salary);
+        System.out.println("JobTitle: "+jobTitle);
+        System.out.println("Location: "+state);
+        System.out.println("==============================================");
     }
+
+
 
 }
