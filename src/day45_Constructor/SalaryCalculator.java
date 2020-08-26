6d5869f0c04796606b82d67e521e19a77d21495f
hasankayman <hasankayman@gmail.com>
2020-08-25 19:01:09 -0600

    Day 45 class

---
+++ src/day45_Constructor/SalaryCalculator.java
@@ -0,0 +1,27 @@
+package day45_Constructor;
+
+public class SalaryCalculator {
+    double hourlyRate;
+    int weeklyHour;
+    double stateTaxRate;
+    double federalTaxRate;
+
+    public SalaryCalculator(double hourlyRate, int weeklyHour, double stateTaxRate, double federalTaxRate) {
+        this.hourlyRate = hourlyRate;
+        this.weeklyHour = weeklyHour;
+        this.stateTaxRate = stateTaxRate;
+        this.federalTaxRate = federalTaxRate;
+    }
+
+    public double salary(){ return hourlyRate * weeklyHour * 48; }
+
+    public double stateTax(){ return salary() * stateTaxRate;}
+
+    public double federalTax() { return salary() * federalTaxRate;}
+
+    public double salaryAfterTax() { return salary() - stateTax() - federalTax();}
+
+    public String toString() {
+        return "Salary: " + salary() + "\nState Tax: " + stateTax() + "\nFederal Tax:" + federalTax() + "\nSalary After Tax: " + salaryAfterTax();
+    }
+}
