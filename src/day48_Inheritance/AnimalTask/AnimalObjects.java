7c2dfe157c323832e3786c6e5cbd17df6b1fb15b
hasankayman <hasankayman@gmail.com>
2020-08-30 10:46:59 -0600

    Day 48 class

---
+++ src/day48_Inheritance/AnimalTask/AnimalObjects.java
@@ -0,0 +1,25 @@
+package day48_Inheritance.AnimalTask;
+
+public class AnimalObjects {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Winston", "Medium", 3, 'M');
+        System.out.println(dog1.name);
+        System.out.println(dog1.size);
+        dog1.bark();
+        dog1.sleep();
+        dog1.eat();
+        System.out.println(dog1.age);
+        System.out.println(dog1.gender);
+        System.out.println(dog1);
+        System.out.println("=================================================");
+        Cat cat1 = new Cat("Seker", "Medium", 1, 'M');
+        System.out.println(cat1.name);
+        System.out.println(cat1.age);
+        cat1.meow();
+        cat1.sleep();
+        cat1.eat();
+        System.out.println(cat1);
+
+    }
+}
---
+++ src/day48_Inheritance/AnimalTask/Dog.java
@@ -0,0 +1,17 @@
+package day48_Inheritance.AnimalTask;
+
+public class Dog extends Animal {
+//           sub          super
+
+    /*
+    variables:4  --> name, age, size, gender
+    methods:3
+    */
+    public void bark() {
+        System.out.println(name + " is barking");
+    }
+
+    public Dog(String name, String size, int age, char gender) {
+        setInfo(name, size, age, gender);
+    }
+}
